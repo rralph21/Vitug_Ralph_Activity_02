5b7f65e175532793510cdee32177eba88a814fc6
Ralph.Vitug <rralph@rrc.ca>
2025-09-22 11:32:48 -0500

    import abstraction and initialized class shape

--- shape/shape.py
+++ shape/shape.py
@@ -1,4 +1,26 @@
 """This module defines the Shape class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Ralph Vitug"
+__version__ = "1.0.0"
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+
+    """
+    Initializes a shape object upon received arguments
+    (if valid).
+
+    args:
+    color (str): Represents color of the shape
+
+    """
+
+    def __init__(self, color: str)
+        
+        if len(color.strip()) == 0:
+            raise ValueError("Color cannot be blank")
+
+        else:
+            self._color = color
+        
